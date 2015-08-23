9b2af73a8213d854b680f6f1cd5f9f9f0e8999c7
nogami <nogami@a.b>
2015-08-23 16:47:08 +0900

    atanをx^5まで展開計算

--- Java/pi/src/pi/CalcAtan.java
+++ Java/pi/src/pi/CalcAtan.java
@@ -1,8 +1,29 @@
 package pi;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/*
+ * atan(1/x)を計算する。
+ */
 public class CalcAtan {
-	public static double atan(double x){
-		double ret=0;
+	public static BigDecimal atan_1divx(int b){
+		BigDecimal a_big = new BigDecimal(1);
+		BigDecimal b_big = new BigDecimal(b);
+		BigDecimal ret = new BigDecimal(0);
+		
+		a_big = a_big.divide(b_big, 10, RoundingMode.HALF_EVEN);
+		ret = ret.add(a_big);
+		
+		//ループ
+		a_big = a_big.divide(b_big, 10, RoundingMode.HALF_EVEN);
+		a_big = a_big.divide(b_big, 10, RoundingMode.HALF_EVEN);
+		ret = ret.subtract(a_big.divide(new BigDecimal(3), 10, RoundingMode.HALF_EVEN));
+		
+		a_big = a_big.divide(b_big, 10, RoundingMode.HALF_EVEN);
+		a_big = a_big.divide(b_big, 10, RoundingMode.HALF_EVEN);
+		ret = ret.add(a_big.divide(new BigDecimal(5)));
+		
 		
 		return ret;
 	}
--- Java/pi/src/pi/CalcPi.java
+++ Java/pi/src/pi/CalcPi.java
@@ -1,11 +1,17 @@
 
 package pi;
 
-import static pi.CalcAtan.atan;
+import static pi.CalcAtan.atan_1divx;
+
+import java.math.BigDecimal;
 
 public class CalcPi {
 	public static void main(String[] argv){
-		double pi=4*atan(1);
+		BigDecimal a=atan_1divx(5).multiply(new BigDecimal(4));
+		BigDecimal b=atan_1divx(239);
+		
+		BigDecimal pi = a.subtract(b).multiply(new BigDecimal(4));
+				
 		System.out.println(pi);
 	}
 }
